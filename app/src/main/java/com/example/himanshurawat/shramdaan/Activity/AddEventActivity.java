2f382ca3a8fe459b94741f166075167036a55afa
Akash Agawal <akashagrawalbest@gmail.com>
2017-10-27 22:21:19 +0530

    Added Activity Classes

---
+++ app/src/main/java/com/example/himanshurawat/shramdaan/Activity/AddEventActivity.java
@@ -0,0 +1,16 @@
+package com.example.himanshurawat.shramdaan.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.himanshurawat.shramdaan.R;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+    }
+}
--- app/src/main/java/com/example/himanshurawat/shramdaan/Activity/CommunityActivity.java
+++ app/src/main/java/com/example/himanshurawat/shramdaan/Activity/CommunityActivity.java
@@ -4,33 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 
 import com.example.himanshurawat.shramdaan.R;
 
-public class ResidentActivity extends AppCompatActivity {
+public class CommunityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_resident);
-        Button residentCommunityButton=findViewById(R.id.activity_resident_community_button);
-        Button residentMCDButton=findViewById(R.id.activity_resident_mcd_button);
-        residentCommunityButton.setOnClickListener(new OnClickListener() {
+        setContentView(R.layout.activity_community);
+        Button addEventButton=findViewById(R.id.activity_community_add_event_button);
+        addEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(ResidentActivity.this,CommunityActivity.class);
+                Intent i=new Intent(CommunityActivity.this,AddEventActivity.class);
                 startActivity(i);
 
             }
         });
-
-
-
-
-
-
-
     }
 }
