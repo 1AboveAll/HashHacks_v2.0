5e0794e341499a970bcabc4362410cbd428c6f85
Himanshu Rawat <rawath54@gmail.com>
2017-10-27 21:24:17 +0530

    Resident Login Click Handled

--- app/src/main/java/com/example/himanshurawat/shramdaan/LoginActivity.java
+++ app/src/main/java/com/example/himanshurawat/shramdaan/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.example.himanshurawat.shramdaan;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.himanshurawat.shramdaan.Activity.ResidentActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button residentLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        residentLoginButton=findViewById(R.id.activity_login_resident_button);
+
+
+
+        residentLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResidentActivity.class));
+            }
+        });
+
     }
 }
